31930affde28fbade315d9147db9d11f0746b9b2
rerishi <rishav.rq@gmail.com>
2023-11-28 15:52:57 +0530

    Just Lookin Like a WOW

--- src/test/java/DimaagKiDahi.java
+++ src/test/java/DimaagKiDahi.java
@@ -2,5 +2,11 @@ public class DimaagKiDahi {
 
 	public static void main(String[] args) {
 		System.out.println("Dimmag ki Dahi ho jaayegi re bawa");
+		
+		System.out.println("So BEAUTIFUL");
+		System.out.println("So ELIGENT");
+		System.out.println("Just Looking Like A WOW");
+		
+		System.out.println("AAYEIN");
 	}
 }
